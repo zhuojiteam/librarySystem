b3a63b04811aad355bc58c9cc2abc8a1d083a82f
lulucici <yolandachen7@zju.edu.cn>
2015-10-16 19:03:13 +0800

    delete html file

---
+++ public/javascripts/menu.js
@@ -0,0 +1,26 @@
+/**
+ * Created by chenyulu on 15/10/16.
+ */
+$(document)
+    .ready(function() {
+
+        // fix menu when passed
+        $('.masthead')
+            .visibility({
+                once: false,
+                onBottomPassed: function() {
+                    $('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function() {
+                    $('.fixed.menu').transition('fade out');
+                }
+            })
+        ;
+
+        // create sidebar and attach to menu open
+        $('.ui.sidebar')
+            .sidebar('attach events', '.toc.item')
+        ;
+
+    })
+;
