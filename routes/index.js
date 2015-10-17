ccdd11c17cdc069510c1a0b743e4ccd7e149a145
Axure <axurez@vip.qq.com>
2015-10-17 17:12:28 +0800

    Separate the view and data.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var hotBooks = require('../services/hot-books');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lulu的图书馆' });
+  res.render('index', { title: 'Lulu的图书馆', bookList: hotBooks });
 });
 
 module.exports = router;
