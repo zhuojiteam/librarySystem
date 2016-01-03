d94158636e332ac36598636c39c40a90dbdf993b
lulucici <yolandachen7@zju.edu.cn>
2016-01-03 19:02:51 +0800

    add data of books in category C

---
+++ data/book_C.js
@@ -0,0 +1,696 @@
+knex('books').insert({
+    title: '娱乐至死  [Amusing ourselves to death]',
+    pub_info: '中信出版集团',
+    isbn: '9787508648286',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '尼尔·波兹曼',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '哈佛非虚构写作课:怎样讲好一个故事  ',
+    pub_info: '中国文史出版社',
+    isbn: '9787503466779',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '马克·克雷默',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '理解媒介：论人的延伸（增订评注本）  ',
+    pub_info: '译林出版社',
+    isbn: '9787544717779',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '马歇尔·麦克卢汉',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '一个人与这个时代：白岩松二十年央视成长记录一个时代变迁备忘  ',
+    pub_info: '上海交通大学出版社',
+    isbn: '9787313103932',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '邹煜',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '巴黎烧了吗？  ',
+    pub_info: '译林出版社',
+    isbn: '9787544736183',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '拉莱·科林斯',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '不要因为走得太远而忘记为什么出发：陈虻，我们听你讲  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300168098',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '徐泓',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '文雅的疯狂  ',
+    pub_info: '上海人民出版社',
+    isbn: '9787208124868',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '尼古拉斯·A. 巴斯贝恩',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '传播学教程（第2版）/普通高等教育“十一五”国家级规划教材·21世纪新闻传播学系列教材  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300111254',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '郭庆光',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '新闻报道与写作（插图第11版）  ',
+    pub_info: '世界图书出版公司',
+    isbn: '9787510065583',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '梅尔文·门彻',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '人民日报传媒书系：一本书学会新闻写作  ',
+    pub_info: '人民日报出版社',
+    isbn: '9787511507204',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '卞丽敏',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '新闻写作教程/21世纪新闻传播学系列教材  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300039817',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '刘明华',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '当代世界学术名著·新闻与传播学译丛·大师经典系列：传播学概论（第2版）  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300122144',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '威尔伯·施拉姆',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '沉默的螺旋：舆论我们的社会皮肤  ',
+    pub_info: '北京大学出版社',
+    isbn: '9787301200346',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '伊丽莎白·诺尔-诺依曼',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '文博教材：中国博物馆学基础（修订本）  ',
+    pub_info: '上海古籍出版社',
+    isbn: '9787532530120',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '王宏钧',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '时评写作十讲  ',
+    pub_info: '复旦大学出版社',
+    isbn: '9787309082029',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '曹林',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '图书编辑校对实用手册（第三版）  ',
+    pub_info: '广西师范大学出版社',
+    isbn: '9787549554799',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '黎洪波',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '古文献学讲义  ',
+    pub_info: '中西书局',
+    isbn: '9787547507117',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '黄永年',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '中文核心期刊要目总览（2014年版）  ',
+    pub_info: '北京大学出版社',
+    isbn: '9787301261897',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '朱强',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '网络传播概论（第3版）/21世纪新媒体专业系列教材  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300162386',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '彭兰',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '新编播音员主持人训练手册：语音发声（第3版 附光盘1张）  ',
+    pub_info: '中国传媒大学出版社',
+    isbn: '9787565711312',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '王璐',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '中国古文献学史（修订本 套装上下册）  ',
+    pub_info: '中华书局',
+    isbn: '9787101100532',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '孙钦善',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '编辑人的世界  [Editors on Editing]',
+    pub_info: '新星出版社',
+    isbn: '9787513311434',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '杰拉尔德·格罗斯',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '做书：感悟与理念  ',
+    pub_info: '商务印书馆',
+    isbn: '9787100115346',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '李昕',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '青少年播音主持训练系列丛书：新编主持人话题宝典  ',
+    pub_info: '中国传媒大学出版社',
+    isbn: '9787565705236',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '王文丽',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '直面媒体20年（平装本）  ',
+    pub_info: '新世界出版社',
+    isbn: '9787510453748',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '赵启正',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '人民日报传媒书系·获奖评论赏析：兼谈评论的写作技巧（最新修订版）  ',
+    pub_info: '人民日报出版社',
+    isbn: '9787511520678',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '刘保全',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '怎样写好文献综述：案例及评述  [How to Review Literature:Cases and Comments]',
+    pub_info: '科学出版社',
+    isbn: '9787030208231',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '张黎',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '新闻的十大基本原则：新闻从业者须知和公众的期待  ',
+    pub_info: '北京大学出版社',
+    isbn: '9787301247518',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '比尔·科瓦奇',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '人民日报传媒书系：通讯员新闻采写一本通  ',
+    pub_info: '人民日报出版社',
+    isbn: '9787511529367',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '王卫明',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '实用粤语播音主持语言基础教程  ',
+    pub_info: '中国广播电视出版社',
+    isbn: '9787504364302',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '刘玉萍',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '复旦新闻与传播学译库·理解新媒介：延伸麦克卢汉  ',
+    pub_info: '复旦大学出版社',
+    isbn: '9787309089530 ',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '罗伯特·洛根',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '新闻编辑学（第三版）（21世纪新闻传播学系列教材·新闻学系列；普通高等教育“十一五”国家级规划教  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300193595',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '蔡雯',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '品牌传播战略：数字时代的整合传播计划  ',
+    pub_info: '科学出版社',
+    isbn: '9787030378774',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '杨东念',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '复旦新闻传播学译库·新媒体系列：新新媒介（第二版）  ',
+    pub_info: '复旦大学出版社',
+    isbn: '9787309105841',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '保罗·莱文森',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '“十二五”普通高等教育本科国家级规划教材：传媒经济学教程  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300103259',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '喻国明',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '博物馆之眼  ',
+    pub_info: '山东美术出版社',
+    isbn: '9787533033064',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '日青',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '复旦博学·新闻与传播学系列教材：新闻学概论（第5版）  ',
+    pub_info: '复旦大学出版社',
+    isbn: '9787309095913',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '李良荣',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '传播理论：起源方法与应用（第5版）/21世纪新闻传播学基础教材  ',
+    pub_info: '中国传媒大学出版社',
+    isbn: '9787810855631',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '赛佛林',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '信息的骨头：数字时代的精准传播  [On Message]',
+    pub_info: '电子工业出版社',
+    isbn: '9787121232497',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '西奥·西奥博尔德',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '人民日报传媒书系：人民日报任仲平80篇  ',
+    pub_info: '人民日报出版社',
+    isbn: '9787511523334',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '人民日报社评论部',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '媒体世界：人类学的新领域  ',
+    pub_info: '商务印书馆',
+    isbn: '9787100102704',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '费·金斯伯格',
+    stock: 10,
+    pub_year: 2013,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '复旦新闻与传播学译库·新媒体系列·媒介、社会与世界：社会理论与数字媒介实践  [Media， Society， World： Social Theory and Digital Media Practice]',
+    pub_info: '复旦大学出版社',
+    isbn: '9787309104387',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '尼克·库尔德利',
+    stock: 10,
+    pub_year: 2011,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '从莎草纸到互联网：社交媒体2000年  [Writing on the Wall]',
+    pub_info: '中信出版集团',
+    isbn: '9787508646121',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '汤姆·斯丹迪奇',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '书店时光：世界最美书店巡礼  [Wonderful Bookstores of the World]',
+    pub_info: '新星出版社',
+    isbn: '9787513310192',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: 'X-Knowledge',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '广播电视新闻采访与写作  ',
+    pub_info: '北京师范大学出版社',
+    isbn: '9787303082711',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '赵淑萍',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '播音主持快速入门十八招儿  ',
+    pub_info: '中国传媒大学出版社',
+    isbn: '9787565701368',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '赵秀环',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '互联网的误读（新闻与传播学译丛·学术前沿系列）  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300188621',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '詹姆斯·柯兰',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '外国新闻传播史/21世纪新闻传播学系列教材  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300033242 ',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '郑超然',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '全媒体时代新闻传播学系列教材：新闻评论教程（第2版）（附学习卡）  ',
+    pub_info: '高等教育出版社',
+    isbn: '9787040325867',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '马少华',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '谷登堡星汉璀璨：印刷文明的诞生  [The Gutenberg Galaxy]',
+    pub_info: '北京理工大学出版社',
+    isbn: '9787564086374',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '马歇尔·麦克卢汉',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '十驾斋养新录  ',
+    pub_info: '上海书店出版社',
+    isbn: '9787806783351',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '钱大昕',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '版权洽谈完全手册  ',
+    pub_info: '世界图书出版公司',
+    isbn: '9787510090882',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '姜汉忠',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '青海年播音主持训练系列丛书：青少年播音主持训练教程（第2版）（附光盘）  ',
+    pub_info: '中国传媒大学出版社',
+    isbn: '9787565707506',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '张洁',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '播音主持语音发声训练教程/21世纪播音主持专业训练教材  ',
+    pub_info: '中国传媒大学出版社',
+    isbn: '9787565701320',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '张涵',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '新媒体十讲  ',
+    pub_info: '中信出版集团',
+    isbn: '9787508652894',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '范卫锋',
+    stock: 10,
+    pub_year: 2015,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '传播学基础：历史、框架与外延（第2版）/普通高等教育“十一五”国家级规划教材  ',
+    pub_info: '中国传媒大学出版社',
+    isbn: '9787565707100',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '段鹏',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '档案管理学（第四版）  ',
+    pub_info: '中国人民大学出版社',
+    isbn: '9787300215075',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: '王英玮',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
+knex('books').insert({
+    title: '信息存储与管理（第2版）：数字信息的存储、管理和保护  ',
+    pub_info: '人民邮电出版社',
+    isbn: '9787115323262',
+    lend_count: 5,
+    total_lend_count: 250,
+    author: 'G.Somasundaram',
+    stock: 10,
+    pub_year: 2012,
+    category_char: 'C',
+    status: 0
+})
