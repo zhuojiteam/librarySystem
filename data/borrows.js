5078153f1dfb375a5f51c123a96c22a123e2c0c9
lulucici <yolandachen7@zju.edu.cn>
2016-01-03 22:57:28 +0800

    add data of borrows

---
+++ data/borrows.js
@@ -0,0 +1,87 @@
+/**
+ * Created by chenyulu on 16/1/3.
+ */
+knex('borrows').insert({
+    user_id: 3,
+    book_id: 35,
+    created_at: new Date(),
+    status: 0
+}),
+knex('borrows').insert({
+    user_id: 3,
+    book_id: 36,
+    created_at: new Date(),
+    status: 1
+}),
+knex('borrows').insert({
+    user_id: 3,
+    book_id: 37,
+    created_at: new Date(),
+    status: 0
+}),
+knex('borrows').insert({
+    user_id: 5,
+    book_id: 38,
+    created_at: new Date(),
+    status: 1
+}),
+knex('borrows').insert({
+    user_id: 6,
+    book_id: 39,
+    created_at: new Date(),
+    status: 0
+}),
+knex('borrows').insert({
+    user_id: 6,
+    book_id: 39,
+    created_at: new Date(),
+    status: 1
+}),
+knex('borrows').insert({
+    user_id: 5,
+    book_id: 38,
+    created_at: new Date(),
+    status: 0
+}),
+knex('borrows').insert({
+    user_id: 1,
+    book_id: 39,
+    created_at: new Date(),
+    status: 0
+}),
+knex('borrows').insert({
+    user_id: 1,
+    book_id: 40,
+    created_at: new Date(),
+    status: 0
+}),
+knex('borrows').insert({
+    user_id: 1,
+    book_id: 41,
+    created_at: new Date(),
+    status: 1
+}),
+knex('borrows').insert({
+    user_id: 2,
+    book_id: 42,
+    created_at: new Date(),
+    status: 0
+}),
+knex('borrows').insert({
+    user_id: 2,
+    book_id: 43,
+    created_at: new Date(),
+    status: 0
+}),
+knex('borrows').insert({
+    user_id: 2,
+    book_id: 44,
+    created_at: new Date(),
+    status: 1
+}),
+knex('borrows').insert({
+    user_id: 5,
+    book_id: 45,
+    created_at: new Date(),
+    status: 0
+})
