f169aead7dba9f5ec49ab80c2ceb5f48355fdfc3
Axure <axurez@vip.qq.com>
2016-01-03 16:38:52 +0800

    Write the user pages for borrows and appointments.

--- routes/users.js
+++ routes/users.js
@@ -14,12 +14,149 @@ var utils = require('../utils');
 //});
 
 router.get('/', function (req, res, next) {
-    res.render('users/b_history', {});
+    var getUserBorrowPromise = models.Borrow
+        .where({
+            user_id: req.user.id
+        })
+        .fetchAll();
+
+    getUserBorrowPromise.then(function (_borrows) {
+        var borrows = _borrows.toJSON();
+        var getBookInfoPromises = [];
+        console.log(borrows);
+        for (var i = 0; i < borrows.length; ++i) {
+            getBookInfoPromises.push(
+                models.Book.where({
+                    id: borrows[i].book_id
+                }).fetch()
+            )
+        }
+        console.log('promise all!');
+        Promise.all(getBookInfoPromises).then(function (_data) {
+            var data = [];
+            for (var i = 0; i < borrows.length; ++i) {
+                var datum = _data[i].toJSON();
+                console.log(datum);
+                datum.created_at = borrows[i].created_at;
+                data.push({
+                    title: datum.title,
+                    author: datum.author,
+                    pub_info: datum.pub_info,
+                    pub_year: datum.pub_year,
+                    isbn: datum.isbn,
+                    status: borrows[i].status,
+                    created_at: borrows[i].created_at
+                });
+            }
+            console.log(data);
+            var stata = {
+                0: {
+                    code: 0,
+                    text: '借阅中'
+                },
+                1: {
+                    code: 1,
+                    text: '已归还'
+                },
+            }
+            var viewData = {};
+            if (req.query.status) {
+                var status = parseInt(req.query.status);
+                data = _.filter(data, function (entry) {
+                    return entry.status == status;
+                });
+                console.log(status);
+                stata[status].active = true;
+                viewData.activeStatus = status;
+            }
+            console.log(stata);
+
+            var pageNumber = (req.query.page) ? parseInt(req.query.page) : 1;
+            var pageSize = 20;
+            var pages = utils.paginate(pageSize, data.length, pageNumber);
+            if (data.length > pageSize) {
+                data = data.slice((pageNumber - 1) * pageSize, (pageNumber) * pageSize)
+            }
+            viewData.pages = pages;
+            viewData.stata = stata;
+            viewData.borrows = data;
+            res.render('users/b_history', viewData);
+        })
+    });
 });
 
 router.get('/a_history', function (req, res, next) {
 
-    res.render('users/a_history', {});
+    var getUserAppointmentPromise = models.Appointment
+        .where({
+            user_id: req.user.id
+        })
+        .fetchAll();
+
+    getUserAppointmentPromise.then(function (_appointments) {
+        var appointments = _appointments.toJSON();
+        var getBookInfoPromises = [];
+        console.log(appointments);
+        for (var i = 0; i < appointments.length; ++i) {
+            getBookInfoPromises.push(
+                models.Book.where({
+                    id: appointments[i].book_id
+                }).fetch()
+            )
+        }
+        console.log('promise all!');
+        Promise.all(getBookInfoPromises).then(function (_data) {
+            var data = [];
+            for (var i = 0; i < appointments.length; ++i) {
+                var datum = _data[i].toJSON();
+                console.log(datum);
+                datum.created_at = appointments[i].created_at;
+                data.push({
+                    title: datum.title,
+                    author: datum.author,
+                    pub_info: datum.pub_info,
+                    pub_year: datum.pub_year,
+                    isbn: datum.isbn,
+                    status: appointments[i].status,
+                    created_at: appointments[i].created_at
+                });
+            }
+            console.log(data);
+            var stata = {
+                0: {
+                    code: 0,
+                    text: '待处理'
+                },
+                1: {
+                    code: 1,
+                    text: '已借阅'
+                },
+            }
+            var viewData = {};
+            if (req.query.status) {
+                var status = parseInt(req.query.status);
+                data = _.filter(data, function (entry) {
+                    return entry.status == status;
+                });
+                console.log(status);
+                stata[status].active = true;
+                viewData.activeStatus = status;
+            }
+            console.log(stata);
+
+            var pageNumber = (req.query.page) ? parseInt(req.query.page) : 1;
+            var pageSize = 20;
+            var pages = utils.paginate(pageSize, data.length, pageNumber);
+            if (data.length > pageSize) {
+                data = data.slice((pageNumber - 1) * pageSize, (pageNumber) * pageSize)
+            }
+            viewData.pages = pages;
+            viewData.stata = stata;
+            viewData.appointments = data;
+            res.render('users/a_history', viewData);
+        })
+    });
+    
 });
 
 router.get('/r_history', function (req, res, next) {
