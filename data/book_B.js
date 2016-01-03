20a6460638e98c74260b5a28c8f050b21640c181
lulucici <yolandachen7@zju.edu.cn>
2016-01-03 18:48:58 +0800

    add data of books in category B

---
+++ data/book_B.js
@@ -0,0 +1,566 @@
+knex('books').insert({
+    title: '退一步，并不难  ',
+    pub_info: '中国商业出版社',
+    isbn: '9787504489975',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '达照',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '哲学家们都干了些什么？  ',
+    pub_info: '北京联合出版公司',
+    isbn: '9787550242234',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '林欣浩',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '你在忙什么  ',
+    pub_info: '民主与建设出版社',
+    isbn: '9787513905855',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '索达吉堪布',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '漫谈弟子规  ',
+    pub_info: '三秦出版社',
+    isbn: '9787551801638',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '索达吉堪布',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '哲学纲要  ',
+    pub_info: '中华书局',
+    isbn: '9787101109986',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '李泽厚',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '阿德勒100句人生革命  ',
+    pub_info: '北京联合出版公司',
+    isbn: '9787550262867',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '阿德勒',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '吴建民谈外交  ',
+    pub_info: '中信出版集团',
+    isbn: '9787508654997',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '吴建民',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '论语译注（典藏版）  ',
+    pub_info: '中华书局',
+    isbn: '9787101107784',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '杨伯峻',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '一切都是最好的安排  ',
+    pub_info: '中国友谊出版公司',
+    isbn: '9787505729230',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '加措',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '与神对话 1（全新修订版）  [CONVERSATIONS WITH GOD: BOOK ONE]',
+    pub_info: '江西人民出版社',
+    isbn: '9787210071976',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '尼尔·唐纳德·沃尔什',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '儒教哲学丛书：天人合一正义  ',
+    pub_info: '中华书局',
+    isbn: '9787101109047',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '蒲创国',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '论语心读（修订本）  ',
+    pub_info: '中华书局',
+    isbn: '9787101111705',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '柳恩铭',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '生死功课  ',
+    pub_info: '北京联合出版公司',
+    isbn: '9787550242791',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: 'Erika',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '图解易经：读懂中国文化第一书（经典图解畅销版）  ',
+    pub_info: '江西科学技术出版社',
+    isbn: '9787539044750',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '高永平',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '心智的力量  ',
+    pub_info: '长江文艺出版公司',
+    isbn: '9787535478900',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '吉杜·克里希那穆提',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '你将成为自己的光  ',
+    pub_info: '长江文艺出版公司',
+    isbn: '9787535478894',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '吉杜·克里希那穆提',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '一切都是最好的安排  ',
+    pub_info: '中国友谊出版公司',
+    isbn: '9787505729230',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '加措',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '觉悟：星云大师的心灵修行课  ',
+    pub_info: '线装书局',
+    isbn: '9787512005334',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '星云大师',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '佛经故事  ',
+    pub_info: '中华书局',
+    isbn: '9787101093186',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '王邦维',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '一切都是最好的安排  ',
+    pub_info: '中国友谊出版公司',
+    isbn: '9787505729230',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '加措',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '中国哲学简史  ',
+    pub_info: '北京大学出版社',
+    isbn: '9787301215692',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '冯友兰',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '中国哲学史（套装上下册）  ',
+    pub_info: '重庆出版社',
+    isbn: '9787229012632',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '冯友兰',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '西藏生死书  ',
+    pub_info: '浙江大学出版社',
+    isbn: '9787308083782',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '索甲仁波切',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '道德经大全集  ',
+    pub_info: '新世界出版社',
+    isbn: '9787510414176',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '老子',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '你的第一本哲学书  [What Does It All Mean]',
+    pub_info: '当代中国出版社',
+    isbn: '9787801703637',
+    lend_count: 10,
+    total_lend_count: 500,
+
+    author: '托马斯·内格尔',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '论语译注  ',
+    pub_info: '中华书局',
+    isbn: '9787101054194',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '杨伯峻',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '礼品装家庭必读书 周易（套装共6册）  ',
+    pub_info: '辽海出版社',
+    isbn: '9787545115376',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '《礼品装家庭必读书》编委会',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '万万没想到 用理工科思维理解世界 入选2014中国好书  ',
+    pub_info: '电子工业出版社',
+    isbn: '9787121242151',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '万维钢',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '心灵的平和之美  ',
+    pub_info: '长江文艺出版社',
+    isbn: '9787535478917',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '克里希那穆提',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '自在  ',
+    pub_info: '甘肃人民美术出版社',
+    isbn: '9787552702910',
+    lend_count: 10,
+    total_lend_count: 500,
+
+    author: '星云大师',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '你将成为自己的光  ',
+    pub_info: '长江文艺出版公司',
+    isbn: '9787535478894',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '吉杜·克里希那穆提',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '龙泉寺学诚法师漫画哲学书：放下是舍更是得+烦恼都是自找的+快乐就这么简单（套装全3册）  ',
+    pub_info: '华文出版社',
+    isbn: '11783474',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '学诚法师',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '狡猾的思想  ',
+    pub_info: '长江文艺出版社',
+    isbn: '9787535478924',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '吉杜·克里希那穆提',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '西方哲学史(套装上下册 网店专供）  ',
+    pub_info: '商务印书馆',
+    isbn: '11757004',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '罗素',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '西方哲学史(套装上下册 网店专供）  ',
+    pub_info: '商务印书馆',
+    isbn: '11757004',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '罗素',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '退一步，并不难  ',
+    pub_info: '中国商业出版社',
+    isbn: '9787504489975',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '达照',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '王阳明心学：典藏修订版  ',
+    pub_info: '民主与建设出版社',
+    isbn: '9787513906708',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '王觉仁',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '论人类不平等的起源和基础  ',
+    pub_info: '浙江文艺出版社',
+    isbn: '9787533941833',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '让-雅克·卢梭',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '万万没想到 用理工科思维理解世界（限量签名版）  ',
+    pub_info: '电子工业出版社',
+    isbn: '9787121242151',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '万维钢',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '阿德勒100句人生革命  ',
+    pub_info: '北京联合出版公司',
+    isbn: '9787550262867',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '阿德勒',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '哲学家们都干了些什么？  ',
+    pub_info: '北京联合出版公司',
+    isbn: '9787550242234',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '林欣浩',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '王阳明最神奇的心学  ',
+    pub_info: '中国华侨出版社',
+    isbn: '9787511327192',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '王阳明<原著>',
+    stock: 30,
+    pub_year: 2014,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '耶鲁大学公开课：死亡  ',
+    pub_info: '北京联合出版公司',
+    isbn: '9787550227392',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '谢利·卡根',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '古兰经+古兰经注（套装共2册）  ',
+    pub_info: '中国社会科学出版社',
+    isbn: '11743510',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '马坚',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '西方哲学史(套装上下册 网店专供）  ',
+    pub_info: '商务印书馆',
+    isbn: '11757004',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '罗素',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '西方哲学史(套装上下册 网店专供）  ',
+    pub_info: '商务印书馆',
+    isbn: '11757004',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '罗素',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
+knex('books').insert({
+    title: '中国哲学简史  ',
+    pub_info: '北京大学出版社',
+    isbn: '9787301215692',
+    lend_count: 10,
+    total_lend_count: 500,
+    author: '冯友兰',
+    stock: 30,
+    pub_year: 2015,
+    category_char: 'B',
+    status: 0
+})
